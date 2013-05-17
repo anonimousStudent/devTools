3fb23133a30b1a2f80a06951cc5b17e7d5c543a3
Aleks Todorov <aleks.k.todorov@gmail.com>
2013-05-17 21:11:08 +0300

    Uplaoding VS project for Development Tools in Git

---
+++ 14.DeveloperTools/1.UsingOfLog4Net/Application.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace _1.UsingOfLog4Net
+{
+    //Imagages for the other tasks can be found in the solution folder. 
+
+    class Application
+    {
+        private static readonly ILog debug = LogManager.GetLogger("Debug");
+        private static readonly ILog exceptions = LogManager.GetLogger("Exceptions");
+
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+            string testMsg = "This is test message!";
+            try
+            {
+                debug.Info("Program starts: ");
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Write(testMsg[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                exceptions.Error(ex.Message);
+            }
+        }
+    }
+}
---
+++ 14.DeveloperTools/2.CreateT4Template/Application.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+class Application
+{
+    static void Main()
+    {
+        //Task 2: Write a simple T4 template of your choice 
+    }
+}
